9c829c3c693702b8f9047287700bddc8a5e99eef
Carol Silva <carolyla@hotmail.com>
2022-07-23 12:46:43 -0300

    feat: :sparkles: criação da rota dos ativos ID

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const ativosController = require('../controllers/ativos.controller');
+const getAtiveById = require('../controllers/ativosId.controller');
 
 const router = express.Router();
 
 router.get('/ativos', ativosController);
+router.get('/ativos/:id', getAtiveById)
 
 module.exports = router;
