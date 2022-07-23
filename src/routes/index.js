73ccdeaa003737cb81a02157f4e9f73474c07d0f
Carol Silva <carolyla@hotmail.com>
2022-07-23 11:57:18 -0300

    fix: :sparkles: alterando as rotas e arquivo index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
-const express = require("express");
+const express = require('express');
+const ativosController = require('../controllers/ativos.controller');
+
 const router = express.Router();
 
+router.get('/ativos', ativosController);
 
 module.exports = router;
