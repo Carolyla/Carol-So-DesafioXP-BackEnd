d383246e91961e87908deba9c8c90e44c6e4da7a
Carol Silva <carolyla@hotmail.com>
2022-07-24 11:25:22 -0300

    feat::sparckles: criando rota de clientes e corrigindo a rota de ativo clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const ativoClienteController = require('../controllers/ativoCliente.controller');
 const ativosController = require('../controllers/ativos.controller');
 const getAtiveById = require('../controllers/ativosId.controller');
+const getClientesController = require('../controllers/clientes.controller');
 
 const router = express.Router();
 
 router.get('/ativos', ativosController);
 router.get('/ativos/:id', getAtiveById);
-router.get('/clientes/:id', ativoClienteController);
+router.get('/clientes/ativos/:id', ativoClienteController);
+router.get('/clientes', getClientesController)
 
 module.exports = router;
