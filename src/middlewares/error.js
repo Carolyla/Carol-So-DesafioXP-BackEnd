88e880b3b9358524f50b6e660a57304d82d54f44
Carol Silva <carolyla@hotmail.com>
2022-07-24 23:00:24 -0300

    feat: middlaware de error

---
+++ src/middlewares/error.js
@@ -0,0 +1,7 @@
+module.exports = (err, req, res, _next) => 
+  res
+    .status(err.status || 500)
+    .json({
+      message: err.message || 'Erro inesperado. Por favor, tente mais tarde',
+      
+    });
