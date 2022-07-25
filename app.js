43b5ce6f84e1c9fcb57c6c61cb6e6a6640018e63
Carol Silva <carolyla@hotmail.com>
2022-07-24 22:53:33 -0300

    feat: implementação do middleware de erros

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require("express-async-errors");
 const express = require("express");
-const router = require('./src/routes')
-
+const router = require("./src/routes");
+const errorMiddleware = require('./src/middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
-
+app.use(errorMiddleware);
 
 module.exports = app;
